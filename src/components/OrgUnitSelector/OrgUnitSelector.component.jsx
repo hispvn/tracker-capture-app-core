3d13d1d52bd0beef47efac0c8633f916ad08a7e7
Nghĩa Nguyễn <nghianguyentran92@gmail.com>
2025-02-07 11:51:06 +0700

    update orgunit selector searching work with user without national orgunit assigned

--- src/components/OrgUnitSelector/OrgUnitSelector.component.jsx
+++ src/components/OrgUnitSelector/OrgUnitSelector.component.jsx
@@ -9,9 +9,18 @@ const OrgUnitSelector = ({ selectedOrgUnit, handleSelectOrgUnit, filter }) => {
   const [orgUnitData, setOrgUnitData] = useState(null);
   const [key, setKey] = useState("");
 
+  const cutString = (str, start) =>
+    str.includes(start) ? str.substring(str.indexOf(start)) : "";
+
   useEffect(() => {
     //metadataApi.getOrgUnitSelectorData(filter).then((json) => {
     metadataApi.getOrgUnitSelectorData().then((json) => {
+      Object.entries(json.tree).forEach(([key, value]) => {
+        const foundRoot = json.roots.find((root) => value.path.includes(root));
+        if (foundRoot) {
+          value.path = cutString(value.path, `/${foundRoot}`);
+        }
+      });
       setOrgUnitData(json);
       setKey(filter.join("") + new Date().getTime());
     });
@@ -26,21 +35,39 @@ const OrgUnitSelector = ({ selectedOrgUnit, handleSelectOrgUnit, filter }) => {
     : "";
   let transformedSelectedOrgUnit = null;
   if (foundSelectedOrgUnit) {
-    if (orgUnitData.roots.includes(selectedOrgUnit)) {
+    if (orgUnitData.roots.includes(selectedOrgUnit?.id)) {
       transformedSelectedOrgUnit = ["/" + foundSelectedOrgUnit.id];
     } else {
       transformedSelectedOrgUnit = [foundSelectedOrgUnit.path];
     }
   }
 
+  const returnFilterWithOuRoots = () => {
+    if (orgUnitData && filter.length > 0) {
+      let newFilter = [];
+      filter.forEach((f) => {
+        const foundRoot = orgUnitData.roots.find((root) => f.includes(root));
+        if (foundRoot) {
+          f = cutString(f, `/${foundRoot}`);
+          newFilter.push(f);
+        }
+      });
+      return newFilter;
+    } else return [];
+  };
+
   const returnOrgUnitTree = () => {
     if (filter.length > 0) {
       return (
         <OrganisationUnitTree
           initiallyExpanded={
-            filter && filter.length > 0 ? filter : orgUnitData.roots
+            filter && filter.length > 0
+              ? returnFilterWithOuRoots()
+              : orgUnitData.roots
           }
-          highlighted={filter && filter.length > 0 ? filter : []}
+          highlighted={
+            filter && filter.length > 0 ? returnFilterWithOuRoots() : []
+          }
           key={key}
           roots={orgUnitData.roots}
           selected={
@@ -49,7 +76,7 @@ const OrgUnitSelector = ({ selectedOrgUnit, handleSelectOrgUnit, filter }) => {
           onChange={(selected) => {
             handleSelectOrgUnit(selected);
           }}
-          filter={filter}
+          filter={returnFilterWithOuRoots()}
         />
       );
     } else {
